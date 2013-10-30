8dcf778b63ffd76a0b2b7fbcf350024655788315
Andrew Wason <rectalogic@rectalogic.com>
2013-10-30 12:30:11 -0400

    Repackage so hl7parser can be installed with pip or easy_install.
    Moved everything into an hl7parser package,
    for backward compatibility existing code can:
        import hl7parser as hl7
    
    unittests and doctests can be run with:
        python setup.py test

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='hl7parser',
+    version='0.5',
+    description='A simple HL7 parser',
+    url='https://github.com/mps-gmbh/hl7-parser',
+    author='Medizinische Planungssysteme GmbH',
+    license='BSD',
+    packages=find_packages(),
+    test_suite='hl7parser.tests',
+    zip_safe=True,
+    classifiers=[
+        'License :: OSI Approved :: BSD License',
+    ],
+)
