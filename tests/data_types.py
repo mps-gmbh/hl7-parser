3db02109e778fe6a7011c7fe7d967b2847d051df
Michael Traub <traub@mps-med.de>
2019-10-01 14:25:44 +0200

    fix(HL7Datetime): Add partial support for old TM format
    If the datetime is given in TM format the second component is
    ignored, because its not reliable and the first component is
    treated like the DTM formatted datetime
    
    fix #23

--- tests/data_types.py
+++ tests/data_types.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 import pytest
 
+from hl7parser.hl7 import HL7Delimiters
 from hl7parser.hl7_data_types import HL7Datetime
 
 
@@ -13,9 +14,12 @@ from hl7parser.hl7_data_types import HL7Datetime
         ("", "", ""),
         ("2010", "2010-01-01T00:00:00", "2010"),
         ("-200", "", ""),
+        ("20190924143134^YYYYMMDDHHMMSS", "2019-09-24T14:31:34", "20190924143134")
     ]
 )
 def test_datetime(input_string, isoformat, string_repr):
-    dt = HL7Datetime(input_string, "|")
+
+    delimiters = HL7Delimiters(*"|^~\&")
+    dt = HL7Datetime(input_string, delimiters)
     assert dt.isoformat() == isoformat
     assert str(dt) == string_repr
