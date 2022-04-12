e9f8d7042f2d44387b7e468263d9f811ef7e4654
RaphaelWuillemier <wuillemier@mps-med.de>
2022-04-12 15:16:03 +0200

    feat(segment): Add insured's address to IN1 segment

--- tests/test_parse.py
+++ tests/test_parse.py
@@ -1,42 +1,44 @@
 #! /usr/bin/python
 # -*- encoding: utf-8 -*-
-from __future__ import unicode_literals
+from __future__ import absolute_import, unicode_literals
 
-from __future__ import absolute_import
-from hl7parser import HL7Message, HL7Segment, HL7Delimiters
-from hl7parser.hl7_data_types import HL7Datetime
-
-import unittest
-import six
-import sys
-import pytest
 import datetime
+import sys
+import unittest
+
+import pytest
+import six
+from hl7parser import HL7Delimiters, HL7Message, HL7Segment
+from hl7parser.hl7_data_types import HL7Datetime
 
 
 @pytest.mark.skipif(sys.version_info.major > 2, reason="not relevant for python 3")
 def test_bytestring_segment():
-    """ bytestring segment can be cast to unicode and bytestring """
+    """bytestring segment can be cast to unicode and bytestring"""
     segment = HL7Segment(b"FOO|Baababamm")
     str(segment)
     six.text_type(segment)
 
+
 @pytest.mark.skipif(sys.version_info.major > 2, reason="not relevant for python 3")
 def test_unicode_segment():
-    """ unicode segment can be cast to unicode and bytestring """
+    """unicode segment can be cast to unicode and bytestring"""
     segment = HL7Segment("FOO|Baababamm")
     str(segment)
     six.text_type(segment)
 
+
 @pytest.mark.skipif(sys.version_info.major > 2, reason="not relevant for python 3")
 def test_bytestring_message():
-    """ bytestring message can be cast to unicode and bytestring """
+    """bytestring message can be cast to unicode and bytestring"""
     message = HL7Message(b"MSH|Dingdong the witch is dead\nFOO|Baababamm")
     str(message)
     six.text_type(message)
 
+
 @pytest.mark.skipif(sys.version_info.major > 2, reason="not relevant for python 3")
 def test_unicode_message():
-    """ unicode message can be cast to unicode and bytestring """
+    """unicode message can be cast to unicode and bytestring"""
     message = HL7Message("MSH|Dingdong the witch is dead\nFOO|Baababamm")
     str(message)
     six.text_type(message)
@@ -44,8 +46,9 @@ def test_unicode_message():
 
 class TestParsing(unittest.TestCase):
     """
-        Test parsing of HL7 messages
+    Test parsing of HL7 messages
     """
+
     msg_string1 = (
         "MSH|^~\&|ADT1|GOOD HEALTH HOSPITAL|GHH LAB, INC.|GOOD HEALTH HOSPITAL|198808181126|SECURITY|ADT^A01^ADT_A01|MSG00001|P^default|2.7|\n"
         "EVN|A01|200708181123||\n"
@@ -69,17 +72,21 @@ class TestParsing(unittest.TestCase):
         "MRG|3150123^^^MSH_MRN^MRN^MTSN^20131016140148^^^^|Q3150123"
     )
 
-    successful_query_result = "\n".join((
-        "MSH|^~\&|pdv|krz|myappl|9270000|201411041444||ADR^A19|0001|P|2.2",
-        "MSA|AA|test234",
-        "QRD|201411041444|R|I|912268|||1^RD|3248239|DEM",
-        "PID|0001||3248239||Raabe^Max|Raabe|19980322|M|||Tiergartenstr. 12^^Berlin^^10785^D||0190/123456^^PH|||||||||||||D||||N",
-    ))
+    successful_query_result = "\n".join(
+        (
+            "MSH|^~\&|pdv|krz|myappl|9270000|201411041444||ADR^A19|0001|P|2.2",
+            "MSA|AA|test234",
+            "QRD|201411041444|R|I|912268|||1^RD|3248239|DEM",
+            "PID|0001||3248239||Raabe^Max|Raabe|19980322|M|||Tiergartenstr. 12^^Berlin^^10785^D||0190/123456^^PH|||||||||||||D||||N",
+        )
+    )
 
-    insuccessful_query_result = "\n".join((
-        "MSH|^~\&|pdv|krz|myappl|9270000|201411041444||ADR^A19|0001|P|2.2",
-        "MSA|AE|test234|2018|||2018^#WHO-FILTER enthält falschen Wert",
-    ))
+    insuccessful_query_result = "\n".join(
+        (
+            "MSH|^~\&|pdv|krz|myappl|9270000|201411041444||ADR^A19|0001|P|2.2",
+            "MSA|AE|test234|2018|||2018^#WHO-FILTER enthält falschen Wert",
+        )
+    )
 
     def test_successful_query_status(self):
         message = HL7Message(self.successful_query_result)
@@ -94,7 +101,7 @@ class TestParsing(unittest.TestCase):
 
     def test_datetime(self):
         message = HL7Message(self.msg_string1)
-        self.assertEqual(six.text_type(message.pid.datetime_of_birth), '19610615')
+        self.assertEqual(six.text_type(message.pid.datetime_of_birth), "19610615")
         delimiters = HL7Delimiters(*"|^~\&")
         old = "18710826"
         dt = HL7Datetime(old, delimiters)
@@ -105,7 +112,10 @@ class TestParsing(unittest.TestCase):
 
     def test_address(self):
         message = HL7Message(self.msg_string1)
-        self.assertEqual(six.text_type(message.pid.patient_address), '&HOME STREET&2^^Greensboro^NC^27401-1020^Westeros')
+        self.assertEqual(
+            six.text_type(message.pid.patient_address),
+            "&HOME STREET&2^^Greensboro^NC^27401-1020^Westeros",
+        )
 
     def test_simple_segments_parse(self):
         message = HL7Message(self.msg_string1)
@@ -126,7 +136,7 @@ class TestParsing(unittest.TestCase):
     def test_trailing_segment_fields(self):
         pid_string = "PID|1||PATID1234^5^M11^ADT1^MR^GOOD HEALTH HOSPITAL~123456789^^^USSSA^SS||EVERYMAN^ADAM^A^III||19610615|M||C|&HOME STREET&2^^Greensboro^NC^27401-1020^Westeros|GL|(555) 555-2004|(555)555-2004|"
         pid = HL7Segment(pid_string)
-        self.assertEqual(six.text_type(pid.ssn_number_patient), '')
+        self.assertEqual(six.text_type(pid.ssn_number_patient), "")
         self.assertEqual(six.text_type(pid), pid_string)
 
     def test_len(self):
@@ -173,10 +183,9 @@ class TestParsing(unittest.TestCase):
         message = HL7Message(self.msg_string1)
 
         # the expected delimiters
-        expected_delimiters = HL7Delimiters('|', '^', '~', '\\', '&')
+        expected_delimiters = HL7Delimiters("|", "^", "~", "\\", "&")
 
-        self.assertEqual(six.text_type(expected_delimiters),
-                         str(message.delimiters))
+        self.assertEqual(six.text_type(expected_delimiters), str(message.delimiters))
 
         # check patient data
         self.assertEqual("ADAM", six.text_type(message.pid.patient_name[0].given_name))
@@ -189,12 +198,19 @@ class TestParsing(unittest.TestCase):
         # check correct parsing of extended composite ID
         self.assertEqual(2, len(message.pid.patient_identifier_list))
 
-        self.assertEqual("GOOD HEALTH HOSPITAL", six.text_type(message.pid.patient_identifier_list[0].assigning_facility))
-        self.assertEqual("123456789", six.text_type(message.pid.patient_identifier_list[1].id_number))
+        self.assertEqual(
+            "GOOD HEALTH HOSPITAL",
+            six.text_type(message.pid.patient_identifier_list[0].assigning_facility),
+        )
+        self.assertEqual(
+            "123456789", six.text_type(message.pid.patient_identifier_list[1].id_number)
+        )
 
         # check correct parsing of extended adress
 
-        self.assertEqual("HOME STREET", six.text_type(message.pid.patient_address[0].street_address.street_name))
+        self.assertEqual(
+            "HOME STREET", six.text_type(message.pid.patient_address[0].street_address.street_name)
+        )
         self.assertEqual("Greensboro", six.text_type(message.pid.patient_address[0].city))
         self.assertEqual("Westeros", six.text_type(message.pid.patient_address[0].country))
 
@@ -218,6 +234,7 @@ class TestParsing(unittest.TestCase):
 
         def invalid_attr():
             message.foobar
+
         self.assertRaises(AttributeError, invalid_attr)
 
     def test_unknown_message_parse(self):
@@ -249,34 +266,27 @@ class TestParsing(unittest.TestCase):
 
         self.assertEqual(six.text_type(segment.patient_class), "I")
 
-        self.assertEqual(
-            six.text_type(segment.assigned_patient_location.point_of_care), "2000")
-        self.assertEqual(
-            six.text_type(segment.assigned_patient_location.room), "2012")
-        self.assertEqual(
-            six.text_type(segment.assigned_patient_location.bed), "01")
+        self.assertEqual(six.text_type(segment.assigned_patient_location.point_of_care), "2000")
+        self.assertEqual(six.text_type(segment.assigned_patient_location.room), "2012")
+        self.assertEqual(six.text_type(segment.assigned_patient_location.bed), "01")
+
+        self.assertEqual(six.text_type(segment.attending_doctor[0].person_identifier), "004777")
+        self.assertEqual(six.text_type(segment.attending_doctor[0].given_name), "AARON")
+        self.assertEqual(six.text_type(segment.attending_doctor[0].family_name), "ATTEND")
+
+        self.assertEqual(six.text_type(segment.attending_doctor[0].second_name), "A")
+
+        self.assertEqual(six.text_type(segment.hospital_service), "SUR")
 
         self.assertEqual(
-            six.text_type(segment.attending_doctor[0].person_identifier), "004777")
-        self.assertEqual(
-            six.text_type(segment.attending_doctor[0].given_name), "AARON")
-        self.assertEqual(
-            six.text_type(segment.attending_doctor[0].family_name), "ATTEND")
-
-        self.assertEqual(
-            six.text_type(segment.attending_doctor[0].second_name), "A")
-
-        self.assertEqual(
-            six.text_type(segment.hospital_service), "SUR")
-
-        self.assertEqual(
-            six.text_type(segment.preadmit_number.effective_date.datetime), "2019-09-24 14:31:34")
+            six.text_type(segment.preadmit_number.effective_date.datetime), "2019-09-24 14:31:34"
+        )
 
 
 def test_in1_segment():
     message_data = (
         "IN1|1:1|McDH|123456789^^^^^^^^^McDonalds Health|McDonalds Health||||||"
-        "|||||||||"
+        "||||||Musterfrau^Gertrud^^^^Dr.^L^A^|SEL|19700101|Königstr. 1B^^Stuttgart^^70173|"
         "||||||||||"
         "||||||1|12345|||||||||||||"
     )
@@ -287,3 +297,24 @@ def test_in1_segment():
     assert str(in1.insurance_company_name) == "McDonalds Health"
 
     assert str(in1.policy_number) == "12345"
+
+    name_of_insured = in1.name_of_insured
+    assert str(name_of_insured) == "Musterfrau^Gertrud^^^^Dr.^L^A^"
+    assert str(name_of_insured.family_name) == "Musterfrau"
+    assert str(name_of_insured.given_name) == "Gertrud"
+    assert str(name_of_insured.middle_name) == ''
+    assert str(name_of_insured.suffix) == ''
+    assert str(name_of_insured.prefix) == ''
+    assert str(name_of_insured.degree) == "Dr."
+    assert str(name_of_insured.name_type_code) == "L"
+    assert str(name_of_insured.name_representation_code) == "A"
+    assert str(name_of_insured.name_context) == ""
+
+    assert str(in1.insureds_relationship_to_patient) == "SEL"
+    assert str(in1.insureds_date_of_birth) == "19700101"
+
+    insureds_address = in1.insureds_address
+    assert str(insureds_address) == "Königstr. 1B^^Stuttgart^^70173"
+    assert str(insureds_address.street_address) == "Königstr. 1B"
+    assert str(insureds_address.city) == "Stuttgart"
+    assert str(insureds_address.zip_or_postal_code) == "70173"
