f8d8ef627059cdb0a57826ee2f6e5732be5473f9
Niklas Meinzer <github@niklas-meinzer.de>
2025-02-13 07:39:17 +0100

    feat(data_types): Add timezone support for HL7Datetime
      Close #31

--- tests/data_types.py
+++ tests/data_types.py
@@ -11,12 +11,15 @@ from hl7parser.hl7_data_types import HL7Datetime
         ("", "", ""),
         ("2010", "2010-01-01T00:00:00", "2010"),
         ("-200", "", ""),
-        ("20190924143134^YYYYMMDDHHMMSS", "2019-09-24T14:31:34", "20190924143134")
+        ("20190924143134^YYYYMMDDHHMMSS", "2019-09-24T14:31:34", "20190924143134"),
+        ("20250204141403.403+0100", "2025-02-04T14:14:03.403000+01:00", "20250204141403.403+0100"),
+        ("20250204141403.03", "2025-02-04T14:14:03.030000", "20250204141403.03")
     ]
 )
 def test_datetime(input_string, isoformat, string_repr):
 
     delimiters = HL7Delimiters(*"|^~\\&")
+
     dt = HL7Datetime(input_string, delimiters)
     assert dt.isoformat() == isoformat
     assert str(dt) == string_repr
